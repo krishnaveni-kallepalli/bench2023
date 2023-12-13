4afe9c0516f629066f0b14b4ffb4c3f60ca4a6d3
krishnaveni-kallepalli <krishnaveni.k@thundersoft.com>
2023-12-13 16:22:16 +0530

    13/12

--- DS/CPP/dll.cpp
+++ DS/CPP/dll.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<stdlib.h>
+#include<math.h>
 using namespace std;
 class Node
 {
@@ -33,6 +34,9 @@ class Double : public Node
 	void delete_begin();
 	void delete_end();
 	void find_middle();
+	void find_mid();
+	void sort_nodes();
+	void count_nodes();
 };
 
 void Double :: insert(int data)
@@ -81,10 +85,67 @@ void Double :: find_middle()
 		p = p->next,q = q->next->next;
 	cout << "Middle element :" << p->data<<endl;
 }
+void Double :: count_nodes()
+{
+	int count = 0;
+	Node *ptr=head;
+	while(ptr!=NULL)
+	{
+		count ++ ;
+		ptr = ptr->next;
+	}
+	cout << "Count:" << count << endl;
+}
+void Double :: find_mid()
+{
+	float count = 0;
+	int pos=0;
+	float mid;
+	Node *ptr = head,*p = head;
+	while(ptr!=NULL)
+	{
+		count ++ ;
+		ptr = ptr->next;
+	}
+	mid  = round(count/2);
+	cout << "Mid:" << mid << endl;
+	while(p->next!=NULL)
+	{
+		pos++;
+		//cout << " pos :" <<pos << endl;
+		if(pos == mid) 
+			printf("Middle:%d\n",p->data);
+		else 
+		{
+			p = p->next;
+		}
+	}
+}
+void Double :: sort_nodes()
+{
+	Node *p1 = head,*p2=NULL;
+	while(p1!=NULL)
+	{
+		p2 = p1->next;
+		while(p2!=NULL)
+		{
+			
+			if(p1->data > p2->data)
+			{
+				int temp;
+				temp = p1->data;
+				p1->data = p2->data;
+				p2->data = temp;
+			}
+			p2 = p2->next;
+		}
+		p1 = p1->next;
+	}
+}
 void Double :: display()
 {
 	Node *ptr = head;
-	cout << "\n	Display:\n";
+	cout << "\nDisplay:\n";
 	if(head == NULL) cout << "No nodes\n";
 	while(ptr != NULL)
 	{
@@ -101,11 +162,13 @@ int main()
 	dll.insert_begin(11);
 	dll.insert_begin(56);
 	dll.display();
-	dll.find_middle();
+	//dll.find_middle();
 	
-	dll.insert(255);
+	dll.insert(25);
+	dll.insert(99);
 	dll.display();
-	dll.find_middle();
+	dll.count_nodes();
+	dll.find_mid();
 	
 	dll.delete_begin();
 	dll.display();
@@ -116,7 +179,8 @@ int main()
 	int ch,data;
 	while(1)
 	{
-		cout << "\n1)Insert\n2)Insert at begin\n3)Delete at begin\n4)Delete at end\n5)Find Middle\n6)Display\n7)Exit\n\n";
+		cout << "\n1)Insert\n2)Insert at begin\n3)Delete at begin\n4)Delete at end\n5)Find Middle\n6)Sort\n7)Count\n\
+8)Find mid\n9)Display\n10)Exit\n\n";
 		cout << "Enter choice:";
 		cin >> ch;
 		switch(ch)
@@ -132,24 +196,43 @@ int main()
 			cin >> data;
 			dll.insert_begin(data);
 			break;
+			
 			case 3:
 			dll.delete_begin();
 			break;
+			
 			case 4:
 			dll.delete_end();
 			break;
+			
 			case 5: 
 			dll.display();
 			dll.find_middle();
 			break;
+			
 			case 6:
 			dll.display();
+			dll.sort_nodes();
+			dll.display();
 			break;
+			
 			case 7:
+			dll.count_nodes();
+			break;
+			case 8:
+			dll.find_mid();
+			break;
+			case 9:
+			dll.display();
+			break;
+			
+			case 10:
 			cout << "Exit..!\n";
 			exit(0);
+			
 			default: cout << "Invalid option\n";
 			exit(1);
 		}
 	}
+
 }
---
+++ c/voting_system.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int ch,A,B,C,D,none;
+	printf("\n1)Group A\n2)Group B\n3)Group C\n4)Group D\n\n");
+	printf("Enter choice:");
+	switch(ch)
+	{
+		case 1:A++;
+		break;
+		case 2:B++;
+		break;
+		case 3:C++;
+		break;
+		case 4:D++;
+		break;
+		default:none ++ ;
+	}
+}
